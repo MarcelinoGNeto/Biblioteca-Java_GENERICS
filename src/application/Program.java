f3a83df06e7fc72b4394e94deedd9cf9a882937f
Marcelino Neto <marcelino.gneto@gmail.com>
2021-08-09 16:49:21 -0300

    Generics - Curinga delimitado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,30 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-
-import entities.Product;
-import services.CalculationService;
 
 public class Program {
-
 	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
 
-		Locale.setDefault(Locale.US);
-		
-		List<Product> list = new ArrayList<>();
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
 
-		String path = "C:\\temp\\ws-sts\\bootcampSantander\\Generics\\in.txt";
-		
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			String line = br.readLine();
-			while (line != null) {
-				String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-				line = br.readLine();
-			}
-			
-			Product x = CalculationService.max(list);
-			System.out.println("Most expensive:");
-			System.out.println(x);
-
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		} 
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 }
