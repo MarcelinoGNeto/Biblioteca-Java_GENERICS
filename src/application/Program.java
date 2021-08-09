dd76e1a7c3da80a7a63b56b6ff0f9721273e790c
Marcelino Neto <marcelino.gneto@gmail.com>
2021-08-09 16:32:49 -0300

    Generics delimitados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,41 @@
 package application;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-import services.PrintService;
+import entities.Product;
+import services.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
 
-		PrintService<Integer> ps = new PrintService<>();
+		String path = "C:\\temp\\ws-sts\\bootcampSantander\\Generics\\in.txt";
 		
-		System.out.print("How many values? ");
-		int n = sc.nextInt();
-		
-		for (int i = 0; i < n; i++) {
-			Integer value = sc.nextInt();
-			ps.addValue(value);
-		}
 
-		ps.print();
-		Integer x = ps.first();
-		System.out.println("First: " + x);
-		
-		sc.close();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.println("Most expensive:");
+			System.out.println(x);
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} 
 	}
 }
