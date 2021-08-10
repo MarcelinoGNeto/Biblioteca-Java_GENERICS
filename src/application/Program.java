ac05842bab88911ae3f9fe050f77b4cb7292f4b5
Marcelino Neto <marcelino.gneto@gmail.com>
2021-08-09 21:30:10 -0300

    Set

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,40 @@
 package application;
 
-import entities.Client;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 	public static void main(String[] args) {
-/*		
-		String a = "Maria";
-		String b = "Alex";
-		System.out.println(a.equals(b)); //false
-		
-		System.out.println("-----------------------------");
-		
-		String c = "Maria";
-		String d = "Alex";
-		System.out.println(c.hashCode()); //74113750
-		System.out.println(d.hashCode()); //2043454
-*/
-		Client c1 = new Client("Maria", "maria@gmail.com");
-		Client c2 = new Client("Maria", "maria@gmail.com");
-		
-		String s1 = new String("Test");
-		String s2 = new String("Test");
-		
-		System.out.println(c1.hashCode());
-		System.out.println(c2.hashCode());
-		System.out.println(c1.equals(c2));
-		System.out.println(c1 == c2);
-		System.out.println(s1 == s2);
+		/*
+		 * //Set<String> set = new HashSet<>(); //Set<String> set = new TreeSet<>();
+		 * Set<String> set = new LinkedHashSet<>();
+		 * 
+		 * set.add("TV"); set.add("Notebook"); set.add("Tablet");
+		 * 
+		 * // set.remove("TV"); //"Notebook" "Tablet" // set.removeIf(x -> x.length() >=
+		 * 3); //remove objetos com mais de 3 caracteres set.removeIf(x -> x.charAt(0)
+		 * == 'T'); //remove todos que tenham a letra T
+		 * 
+		 * for (String p : set) { System.out.println(p); }
+		 */
 
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+		
+		// union - união
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
+		
+		// intersection - interseção
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
+		
+		// difference - diferença
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 	}
 }
