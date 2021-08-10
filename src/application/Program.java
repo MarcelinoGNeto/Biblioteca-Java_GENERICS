b0969ebd791933dba11523357a7b05eb838212d7
Marcelino Neto <marcelino.gneto@gmail.com>
2021-08-09 22:13:29 -0300

    Exercicio de fixacao Set

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,45 +1,47 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
+import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-import entities.LogEntry;
-
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter file full path: ");
-		String path = sc.nextLine();
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			Set<LogEntry> set = new HashSet<>();
-
-			String line = br.readLine();
-			while (line != null) {
-
-				String[] fields = line.split(" ");
-				String username = fields[0];
-				Date moment = Date.from(Instant.parse(fields[1]));
-
-				set.add(new LogEntry(username, moment));
-
-				line = br.readLine();
-			}
-			System.out.println("Total users: " + set.size());
-
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
+		
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
 		}
+
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			b.add(number);
+		}
+
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			c.add(number);
+		}
+		
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+
+		System.out.println("Total students: " + total.size());
+
 		sc.close();
 	}
 }
